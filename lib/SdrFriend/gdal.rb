36f9164532f78d1a7440ea116e3e2e9af2945986
Stephen Balogh <sgbalogh@gmail.com>
2018-05-16 13:30:37 -0400

    update gitignore, comment gdal method

--- lib/SdrFriend/gdal.rb
+++ lib/SdrFriend/gdal.rb
@@ -13,5 +13,13 @@ module SdrFriend
       return SdrFriend::Metadata.create_solr_geom(coords[0], coords[2], coords[3], coords[1])
     end
 
+    # def self.shapefile_to_wgs84shp(input_shapefile_path, output_shapefile_path, verbose=true)
+    #   if verbose
+    #     puts "Beginning conversion from:\n\t#{input_shapefile_path}\nto\n\t#{output_shapefile_path}"
+    #   end
+    #   result = `ogr2ogr -t_srs EPSG:4326 "#{output_shapefile_path}" "#{input_shapefile_path}"`
+    #   return result
+    # end
+
   end
 end
